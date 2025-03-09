1967c5d754f632ec2a89c63b142d9563d23a0147
Cesar Daniel <cesardaniel.9611@gmail.com>
2025-03-08 21:27:27 -0500

    starting point for popover with swiperjs implementation
    Good luck.

--- swiper.js
+++ swiper.js
@@ -1,9 +1,19 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: 'vertical',
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+document.querySelectorAll(".swiper-slide").forEach((slide) => {
+  slide.addEventListener("click", () => {
+    const popover = document.getElementById("image-popover");
+    const popoverImage = document.getElementById("popover-image");
+    popoverImage.src = slide.src;
+    popover.togglePopover();
   });
+});
+
