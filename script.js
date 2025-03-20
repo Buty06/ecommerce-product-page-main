243cc8beafd76508bdc270757194d18fb621275a
Buty06 <mecagoenlashormigas@gmail.com>
2025-03-20 17:28:57 -0400

    Refactorizando un poco el codigo y creando la integracion de cierre tambien en el main

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-const openMenu = () => {
-  const openButton = document.getElementById("menu_logo");
-  const clouseButton = document.getElementById("clouse_menu_btn");
-  const aside = document.getElementById("aside");
+const openButton = document.getElementById("menu_logo");
+const clouseButton = document.getElementById("clouse_menu_btn");
+const main = document.getElementById("main");
+const aside = document.getElementById("aside");
 
-  openButton.addEventListener("click", () => {
-    aside.style.display = "grid";
-    aside.classList.remove("sliderIn");
-    aside.classList.add("sliderOff");
-  });
+openButton.addEventListener("click", (e) => {
+  aside.style.display = "grid";
+  aside.classList.remove("sliderIn");
+  aside.classList.add("sliderOff");
+});
 
-  clouseButton.addEventListener("click", () => {
-    aside.classList.remove("sliderOff");
+const clouseAside = () => {
+  aside.classList.remove("sliderOff");
+  aside.classList.add("sliderIn");
 
-    aside.classList.add("sliderIn");
-
-    setTimeout(() => {
-      aside.style.display = "none";
-    }, 950);
-  });
+  setTimeout(() => {
+    aside.style.display = "none";
+  }, 950);
 };
 
-openMenu();
+clouseButton.addEventListener("click", clouseAside);
+main.addEventListener("click", clouseAside);
