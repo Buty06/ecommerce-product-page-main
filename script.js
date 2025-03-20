5f68a104224b597cb41325c9a3f0912bf99e0eb3
Buty06 <mecagoenlashormigas@gmail.com>
2025-03-20 17:18:11 -0400

    Creando la animacion del menu pero le falta funcionalidad

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const openMenu = () => {
   const openButton = document.getElementById("menu_logo");
+  const clouseButton = document.getElementById("clouse_menu_btn");
   const aside = document.getElementById("aside");
 
   openButton.addEventListener("click", () => {
     aside.style.display = "grid";
+    aside.classList.remove("sliderIn");
+    aside.classList.add("sliderOff");
+  });
+
+  clouseButton.addEventListener("click", () => {
+    aside.classList.remove("sliderOff");
+    aside.classList.add("sliderIn");
+
+    setTimeout(() => {
+      aside.style.display = "none";
+    }, 950);
   });
 };
 
